bcf4bd96b7b20b4eb4fda4e3dd54ef71ad9822c4
ahmad <ahmadalshelleh@yahoo.com>
2022-10-09 15:21:48 +0300

    latest

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Test {window.location.href} CI/CD 765765 123123 00 ahm</h1>
+      <h1>Test {window.location.href} CI/CD 765765 123123 00 ahmad</h1>
     </div>
   );
 }
