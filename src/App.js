60630e083da3a17d09aedd0d7dcac7af2514258c
ahmad <ahmadalshelleh@yahoo.com>
2022-10-09 14:13:41 +0300

    latest

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Test $(hostname -f) CI/CD 765765 123123 00 ahm</h1>
+      <h1>Test {window.location.href} CI/CD 765765 123123 00 ahm</h1>
     </div>
   );
 }
